3fdc2521fc7692df08c3687c744fbffbcc81988e
Yukami <akumahoneko@gmail.com>
2022-05-22 14:16:07 +0700

    Nothing

--- src/main/java/com/yukami/firstmod/FirstMod.java
+++ src/main/java/com/yukami/firstmod/FirstMod.java
@@ -1,10 +1,12 @@
 package com.yukami.firstmod;
 
 import com.mojang.logging.LogUtils;
+import com.yukami.firstmod.Item.ModItems;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
@@ -27,8 +29,11 @@ public class FirstMod
 
     public FirstMod()
     {
-        // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(eventBus);
+
+        eventBus.addListener(this::setup);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
