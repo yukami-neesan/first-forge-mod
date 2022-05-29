bb2f19106f9aab97a6a406d84643237b1e3a3cb7
Yukami <akumahoneko@gmail.com>
2022-05-29 20:58:55 +0700

    wow

--- src/main/java/com/yukami/firstmod/item/ModItems.java
+++ src/main/java/com/yukami/firstmod/item/ModItems.java
@@ -3,8 +3,8 @@ package com.yukami.firstmod.item;
 import com.yukami.firstmod.FirstMod;
 import com.yukami.firstmod.item.custom.CoalCokeItem;
 import com.yukami.firstmod.item.custom.EnlighteningRod;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.SwordItem;
+import com.yukami.firstmod.item.custom.Excalibur;
+import net.minecraft.world.item.*;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -21,10 +21,10 @@ public class ModItems {
 
     public static final RegistryObject<Item> TUNGSTEN_ORE = ITEMS.register("raw_tungsten", () -> new Item(new Item.Properties().tab(ModCreativeTab.MAGIC_TAB)));
 
-    public static final RegistryObject<Item> TUNGSTEN_SWORD = ITEMS.register("tungsten_sword", () -> new SwordItem(ModTiers.TUNGSTEN, 2, 3f, new Item.Properties().tab(ModCreativeTab.MAGIC_TAB)));
-    public static final RegistryObject<Item> TUNGSTEN_PICKAXE = ITEMS.register("tungsten_pickaxe", () -> new SwordItem(ModTiers.TUNGSTEN, 2, 3f, new Item.Properties().tab(ModCreativeTab.MAGIC_TAB)));
-    public static final RegistryObject<Item> TUNGSTEN_AXE = ITEMS.register("tungsten_axe", () -> new SwordItem(ModTiers.TUNGSTEN, 2, 3f, new Item.Properties().tab(ModCreativeTab.MAGIC_TAB)));
-    public static final RegistryObject<Item> TUNGSTEN_HOE = ITEMS.register("tungsten_hoe", () -> new SwordItem(ModTiers.TUNGSTEN, 2, 3f, new Item.Properties().tab(ModCreativeTab.MAGIC_TAB)));
+    public static final RegistryObject<Item> TUNGSTEN_SWORD = ITEMS.register("tungsten_sword", () -> new Excalibur(ModTiers.TUNGSTEN, 2, 3f, new Item.Properties().tab(ModCreativeTab.MAGIC_TAB)));
+    public static final RegistryObject<Item> TUNGSTEN_PICKAXE = ITEMS.register("tungsten_pickaxe", () -> new PickaxeItem(ModTiers.TUNGSTEN, 2, 3f, new Item.Properties().tab(ModCreativeTab.MAGIC_TAB)));
+    public static final RegistryObject<Item> TUNGSTEN_AXE = ITEMS.register("tungsten_axe", () -> new AxeItem(ModTiers.TUNGSTEN, 2, 3f, new Item.Properties().tab(ModCreativeTab.MAGIC_TAB)));
+    public static final RegistryObject<Item> TUNGSTEN_HOE = ITEMS.register("tungsten_hoe", () -> new HoeItem(ModTiers.TUNGSTEN, 2, 3f, new Item.Properties().tab(ModCreativeTab.MAGIC_TAB)));
 
     public static void register(IEventBus eventBus){
         ITEMS.register(eventBus);
